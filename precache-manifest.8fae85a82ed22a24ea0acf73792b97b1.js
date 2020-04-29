f6fa4343c6ba1ed4e95901f833ec7718cfd733c0
Ethan <33491345+Tigernater@users.noreply.github.com>
2020-04-29 17:43:14 +0100

    Updates

---
+++ precache-manifest.8fae85a82ed22a24ea0acf73792b97b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b56c34b58edd2cf7cb8f53e096d69db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18df55f332bb192bfc15",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "8c6da9ceb6a5c1680b04",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "18df55f332bb192bfc15",
+    "url": "/static/js/2.009cf1c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.009cf1c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c6da9ceb6a5c1680b04",
+    "url": "/static/js/main.d4e3867b.chunk.js"
+  },
+  {
+    "revision": "46b2ab5944883490d30d",
+    "url": "/static/js/runtime-main.501fdd23.js"
+  }
+]);
